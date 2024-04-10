4a2a119212d5ba7bf487e68c9277337afdaade2a
Miguel Fernández Fuentes <miguel.fernandez@codurance.com>
2024-04-10 18:42:18 +0200

    Loop iteration for command

--- test/mars-rover.spec.ts
+++ test/mars-rover.spec.ts
@@ -2,23 +2,25 @@ import {MarsRover} from "../src/mars-rover";
 
 describe('MarsRover', () => {
 
+    let marsRover: MarsRover
+    beforeEach(() => {
+        marsRover = new MarsRover()
+
+    })
+
     it('should rotate right', () => {
-        const marsRover: MarsRover = new MarsRover()
         expect(marsRover.execute('R')).toBe('0:0:E')
     })
 
     it('should rotate right twice', () => {
-        const marsRover: MarsRover = new MarsRover()
         expect(marsRover.execute('RR')).toBe('0:0:S')
     })
 
     it('should rotate right thrice', () => {
-        const marsRover: MarsRover = new MarsRover()
         expect(marsRover.execute('RRR')).toBe('0:0:W')
     })
 
     it('should rotate four times', () => {
-        const marsRover: MarsRover = new MarsRover()
         expect(marsRover.execute('RRRR')).toBe('0:0:N')
     })
 
