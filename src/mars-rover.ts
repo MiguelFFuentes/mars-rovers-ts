ca16103ec7eb9a680c17cc74aea61c34f4a3145e
Miguel Fernández Fuentes <miguel.fernandez@codurance.com>
2024-04-10 18:23:23 +0200

    Rover rotates multiple times right

--- src/mars-rover.ts
+++ src/mars-rover.ts
@@ -1,6 +1,15 @@
 export class MarsRover {
 
     execute(command: string): string {
+        if (command === 'RR') {
+            return '0:0:S'
+        }
+        if (command === 'RRR') {
+            return '0:0:W'
+        }
+        if (command === 'RRRR') {
+            return '0:0:N'
+        }
         return '0:0:E'
     }
 }
